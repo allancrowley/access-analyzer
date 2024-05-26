8ee181996bd09eaebd907f90e6c5a17b03648d86
allancrowley <allancrowleydev@gmail.com>
2024-05-26 12:06:59 +0300

    project modules update

--- ip-analyzer/src/main/java/com/access/IpAnalyzerApplication.java
+++ ip-analyzer/src/main/java/com/access/IpAnalyzerApplication.java
@@ -1,13 +1,37 @@
 package com.access;
 
+import com.access.dto.AuthFailureDto;
+import com.access.service.IpAnalyzerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 public class IpAnalyzerApplication {
+    @Value("${app.analyzer.producer.binding.name}")
+    String producerBindingName;
+    final IpAnalyzerService ipAnalyzerService;
+
 
     public static void main(String[] args) {
         SpringApplication.run(IpAnalyzerApplication.class, args);
     }
 
+    @Bean
+    Consumer<AuthFailureDto> analyzerConsumer() {
+        return this::processAuthFailure;
+    }
+
+    private void processAuthFailure(AuthFailureDto authFailureDto) {
+        log.trace("received authentication failure data: {}", authFailureDto);
+        ipAnalyzerService.processAuthFailure(authFailureDto, producerBindingName);
+
+    }
 }
