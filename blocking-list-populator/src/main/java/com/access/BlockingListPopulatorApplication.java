570861801ae98b1ccd0d2704db4cc7f68fa23755
allancrowley <allancrowleydev@gmail.com>
2024-05-26 11:52:27 +0300

    project modules update

--- blocking-list-populator/src/main/java/com/access/BlockingListPopulatorApplication.java
+++ blocking-list-populator/src/main/java/com/access/BlockingListPopulatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlockingListPipulatorApplication {
+public class BlockingListPopulatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlockingListPipulatorApplication.class, args);
+        SpringApplication.run(BlockingListPopulatorApplication.class, args);
     }
 
 }
--- blocking-list-populator/src/test/java/com/access/blockinglistpipulator/BlockingListPopulatorApplicationTests.java
+++ blocking-list-populator/src/test/java/com/access/blockinglistpipulator/BlockingListPopulatorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlockingListPipulatorApplicationTests {
+class BlockingListPopulatorApplicationTests {
 
     @Test
     void contextLoads() {
