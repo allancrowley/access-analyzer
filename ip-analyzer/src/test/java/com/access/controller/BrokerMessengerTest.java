af46b23f99857fd98242279922678e78d5ef9904
Alexander <caxagsh@gmail.com>
2024-06-18 22:04:22 +0300

    ip-analyzer final tests ver1.1 and new IpAnalyzerApplication.class

--- ip-analyzer/src/test/java/com/access/controller/BrokerMessengerTest.java
+++ ip-analyzer/src/test/java/com/access/controller/BrokerMessengerTest.java
@@ -17,17 +17,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.stream.binder.test.InputDestination;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
-
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
-
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.function.Consumer;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -52,33 +49,34 @@ public class BrokerMessengerTest  {
     private IpAnalyzerApplication application;
 
 
-
+    ObjectMapper objectMapper = new ObjectMapper();
 
 
     @Test
     @DisplayName("Test kafka send AuthFailureDto")
-    void kafkaSendAuthFailureDto()  {
-        AuthFailureDto authFailureDto = DataUtils.getFullMapAuthFailureDto();
-        producer.send(new GenericMessage<>(authFailureDto));
+    void givenAuthFailureDto_whenKafkaSendAuthFailureDto_thenOk() throws IOException  {
+        //given
+        AuthFailureDto authFailureDto = DataUtils.getExistsAuthFailureDto();
+        //when
+        producer.send(new GenericMessage<>(authFailureDto), "auth-failure");
+        //then
         verify(ipAnalyzerService, times(1)).processAuthFailure(any(AuthFailureDto.class));
-
     }
 
     @Test
     @DisplayName("Test processAuthFailure cold and  send message")
-    void processAuthFailureColdAndSendMessage() throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-
-        Method method = IpAnalyzerApplication.class.getDeclaredMethod("process-AuthFailure");
-        method.setAccessible(true);
+    void givenAuthFailureDto_whenIpAnalyzerServiceCold_thenAttackAttemptDtoReturnedAndMessageSend() throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        //given
         AuthFailureDto authFailureDto = DataUtils.getExistsAuthFailureDto();
-        method.invoke(application, authFailureDto);
-        var message = this.consumer.receive(10, producerBindingName);
-        ObjectMapper objectMapper = new ObjectMapper();
-        AttackAttemptDto attackAttemptDto = objectMapper.readValue(message.getPayload(), AttackAttemptDto.class);
-        String actualSubnet = attackAttemptDto.subnet();
-        assertEquals("100.100.100", actualSubnet);
+        AttackAttemptDto attackAttemptDto = DataUtils.getAttackAttemptDto();
+        //when
+        when(ipAnalyzerService.processAuthFailure(authFailureDto)).thenReturn(attackAttemptDto);
+        producer.send(new GenericMessage<>(authFailureDto), "auth-failure");
+        Message<byte[]> message = consumer.receive(10, "attack-out-0");
+        AttackAttemptDto dto = objectMapper.readValue(message.getPayload(), AttackAttemptDto.class);
+        //then
+        assertEquals("100.100.100", dto.subnet());
+        assertNotNull(message);
     }
-
-
 }
 
