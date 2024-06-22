f95383afc27d351f90647ddd88af010aa5e4b091
Daniel Kleyman <denny_klm1@yahoo.com>
2024-06-22 16:31:36 +0300

    adds repository test

--- blocked-ip-provider/src/test/java/repo/BlockedIpProviderRepositoryTest.java
+++ blocked-ip-provider/src/test/java/repo/BlockedIpProviderRepositoryTest.java
@@ -1,20 +1,14 @@
 package com.access.repo;
 
 import com.access.model.IpSubnetEntity;
-import com.access.repo.AccessDb;
-import com.access.repo.AccessSQLImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.TestPropertySource;
 import util.DataUtils;
 
-import javax.xml.crypto.Data;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -23,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 @ComponentScan("com.access.repo")
-public class BlockedIpProviderRepositaryTest {
+public class BlockedIpProviderRepositoryTest {
     @Autowired
     private AccessDb accessDb;
 
@@ -46,8 +40,5 @@ public class BlockedIpProviderRepositaryTest {
         //then
         assertThat(ips).isNotNull();
         assertEquals(ips, expectedIps);
-
     }
-
-
 }
