ece60fc69c0b7d5f62fd6fd91620208bdceb4847
Alexander <caxagsh@gmail.com>
2024-06-30 21:33:02 +0300

    data-extractor with WebClient ver 1.0

--- data-extractor/src/main/java/com/access/DataExtractorApplication.java
+++ data-extractor/src/main/java/com/access/DataExtractorApplication.java
@@ -1,7 +1,10 @@
 package com.access;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DataExtractorApplication {
@@ -9,5 +12,6 @@ public class DataExtractorApplication {
     public static void main(String[] args) {
         SpringApplication.run(DataExtractorApplication.class, args);
     }
+
 
 }
