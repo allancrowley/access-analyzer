6dca8e4a24a62140e2c38950b1538790991c59a4
allancrowley <allancrowleydev@gmail.com>
2024-07-01 19:55:25 +0300

    adds customer-auth module (initial commit)

---
+++ customer-auth/src/main/java/com/access/CustomerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAuthApplication.class, args);
+	}
+
+}
